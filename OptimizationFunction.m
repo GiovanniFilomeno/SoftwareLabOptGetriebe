03fac4291d45a4b3608c467ff81803447e273f63
d4r10-tum <dario.arreguin@tum.de>
2021-11-10 23:21:35 +0100

    Update OptimizationFunction.m

--- OptimizationFunction.m
+++ OptimizationFunction.m
@@ -1,15 +1,24 @@
 function fitness = OptimizationFunction(bauraum_gearbox, EC1_center, EC1_SurfCenter, EC2_SurfCenter, EC3_SurfCenter, EC4_SurfCenter, EM_start, EM_end, in1_bauraum, in2_bauraum, in3_bauraum, in4_bauraum, out1_gearbox,  out2_gearbox, out3_gearbox, out4_gearbox, collision12, collision13, collision14, collision23, collision24, collision34, EC2_placement) 
-    Pen_gearbox = 10000*5;
+    Pen_gearbox = 5000*5;
     Pen_bauraum = 5000*5;
     Pen_collision = 5000*5;
+    Pen_distance = 1000;
+    min_distance = 30;
     
+%     EC1_SurfCenter(1,1) = EC1_SurfCenter(1,1) + 10;
+%     EC1_SurfCenter(2,1) = EC1_SurfCenter(2,1) - 10;
+%     EC2_SurfCenter(1,1) = EC2_SurfCenter(1,1) + 10;
+%     EC2_SurfCenter(2,1) = EC2_SurfCenter(2,1) - 10;
+%     EC3_SurfCenter(1,1) = EC3_SurfCenter(1,1) + 10;
+%     EC3_SurfCenter(2,1) = EC3_SurfCenter(2,1) - 10;
+
     if bauraum_gearbox ~= 0.9375
         Pen_baugb = 1000;
     else
         Pen_baugb = 0;
     end
-    
-    
+        
+
     %IGBT to Electric motor
     if EC1_center(1,2) > EM_start(1,2) || EC1_center(1,2) < EM_end(1,2)
          distance = point_to_line(EC1_center, EM_start,EM_end);
@@ -24,23 +33,38 @@ function fitness = OptimizationFunction(bauraum_gearbox, EC1_center, EC1_SurfCen
     if EC2_placement == 1       
         %IGBT to condensator
             distance = norm(EC1_SurfCenter(1,:) - EC2_SurfCenter(2,:));
+            if distance < min_distance
+                distance = distance*Pen_distance;
+            end
             fitness2 = distance + (1-in2_bauraum)*Pen_bauraum +(out2_gearbox)*Pen_gearbox;
         %Condensator to EMI-filter    
             distance = norm(EC2_SurfCenter(1,:) - EC3_SurfCenter(2,:));
+            if distance < min_distance
+                distance = distance*Pen_distance;
+            end
             fitness3 = distance + (1-in3_bauraum)*Pen_bauraum +(out3_gearbox)*Pen_gearbox;
             
     %Clockwise placement of Condensator
     else
         %IGBT to condensator
             distance = norm(EC1_SurfCenter(2,:) - EC2_SurfCenter(1,:));
+            if distance < min_distance
+                distance = distance*Pen_distance;
+            end
             fitness2 = distance + (1-in2_bauraum)*Pen_bauraum +(out2_gearbox)*Pen_gearbox;
         %Condensator to EMI-filter    
             distance = norm(EC2_SurfCenter(2,:) - EC3_SurfCenter(1,:));
+            if distance < min_distance
+                distance = distance*Pen_distance;
+            end
             fitness3 = distance + (1-in3_bauraum)*Pen_bauraum +(out3_gearbox)*Pen_gearbox;
     end
         
-%IGBT to Control board    
+    %IGBT to Control board    
     distance = norm(EC1_SurfCenter(3,:) - EC4_SurfCenter(4,:));
+    if distance < min_distance
+                distance = distance*Pen_distance;
+    end
     fitness4 = distance + (1-in4_bauraum)*Pen_bauraum +(out4_gearbox)*Pen_gearbox;
     
     
