ad3b4206ce28b1b020d6bbcbda140e3201e19a14
d4r10-tum <dario.arreguin@tum.de>
2021-11-10 23:20:16 +0100

    Update EvaluateFitness.m

--- EvaluateFitness.m
+++ EvaluateFitness.m
@@ -1,4 +1,4 @@
-function Fitness = EvaluateFitness(g,pop,bauraum,gearbox,EC1,EC1_width, EC1_center,EC1_SurfCenter,EC2,EC2_center,EC2_SurfCenter,EC3,EC3_center,EC3_SurfCenter,EC4,EC4_center,EC4_SurfCenter,Anchor,EM_start,EM_vector,r,y,theta,Rotations,EC2_placement, Fitness)
+function Fitness = EvaluateFitness(Mean_fitness, iter, g,pop,bauraum,gearbox,EC1,EC1_width, EC1_center,EC1_SurfCenter,EC2,EC2_center,EC2_SurfCenter,EC3,EC3_center,EC3_SurfCenter,EC4,EC4_center,EC4_SurfCenter,Anchor,EM_start,EM_vector,r,y,theta,Rotations,EC2_placement, Fitness)
 
 
 for i=1:pop
@@ -152,126 +152,127 @@ for i=1:pop
 %         figure(i+1)
 %         Visualize(bauraum,gearbox,EC1,EC2,EC3,EC4);
 %         hold on
-%     end
-       
+%     end   
 
     Clearance = 5;
-     if Fitness(i,1) < 200
+     if g > iter/2
+         if Fitness(i,1) < Mean_fitness/10
 %         figure(i+1)
 %         Visualize(bauraum,gearbox,EC1,EC2,EC3,EC4);
 %         hold on
-        if EC2_placement(i,1) == 1   %Counterclockwise        
-            vector1 = EC1_SurfCenter(2,:) - EC1_SurfCenter(1,:);
-            vector2 = EC2_SurfCenter(1,:) - EC2_SurfCenter(2,:);
-            pt2 = EC1_SurfCenter(1,:) - (Clearance.*vector1)/norm(vector1);
-            pt3 = EC2_SurfCenter(2,:) - (Clearance.*vector2)/norm(vector2);
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(1,:), pt2, pt3, EC2_SurfCenter(2,:),EC1_center, EC2_center, EC1, EC2, cable_radius, radius_of_curvature);
-            Fitness(i,1) = Fitness(i,1) + Penalty_spline;
-            vector1 = EC2_SurfCenter(2,:) - EC2_SurfCenter(1,:);
-            vector2 = EC3_SurfCenter(1,:) - EC3_SurfCenter(2,:);
-            pt2 = EC2_SurfCenter(1,:) - (Clearance.*vector1)/norm(vector1);
-            pt3 = EC3_SurfCenter(2,:) - (Clearance.*vector2)/norm(vector2);
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC2_SurfCenter(1,:), pt2, pt3, EC3_SurfCenter(2,:),EC2_center, EC3_center, EC2, EC3, cable_radius, radius_of_curvature);
-            Fitness(i,1) = Fitness(i,1) + Penalty_spline;
-            vector1 = EC1_SurfCenter(4,:) - EC1_SurfCenter(3,:);
-            vector2 = EC4_SurfCenter(3,:) - EC4_SurfCenter(4,:);
-            pt2 = EC1_SurfCenter(3,:) - (Clearance.*vector1)/norm(vector1);
-            pt3 = EC4_SurfCenter(4,:) - (Clearance.*vector2)/norm(vector2);
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(3,:), pt2, pt3, EC4_SurfCenter(4,:),EC1_center, EC4_center, EC1, EC4, cable_radius, radius_of_curvature);
-            Fitness(i,1) = Fitness(i,1) + Penalty_spline;
-            
-            EM_connection = EM_end;
-            clearance_mid = 20;
-            clearance_posy = 30;
-            clearance_negy = 10;
-            rad_gb = EM_radius;
-            vector1 = EC1_SurfCenter(2,:) - EC1_SurfCenter(1,:);
+            if EC2_placement(i,1) == 1   %Counterclockwise        
+                vector1 = EC1_SurfCenter(2,:) - EC1_SurfCenter(1,:);
+                vector2 = EC2_SurfCenter(1,:) - EC2_SurfCenter(2,:);
+                pt2 = EC1_SurfCenter(1,:) - (Clearance.*vector1)/norm(vector1);
+                pt3 = EC2_SurfCenter(2,:) - (Clearance.*vector2)/norm(vector2);
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(1,:), pt2, pt3, EC2_SurfCenter(2,:),EC1_center, EC2_center, EC1, EC2, bauraum, cable_radius, radius_of_curvature);
+                Fitness(i,1) = Fitness(i,1) + Penalty_spline;
+                vector1 = EC2_SurfCenter(2,:) - EC2_SurfCenter(1,:);
+                vector2 = EC3_SurfCenter(1,:) - EC3_SurfCenter(2,:);
+                pt2 = EC2_SurfCenter(1,:) - (Clearance.*vector1)/norm(vector1);
+                pt3 = EC3_SurfCenter(2,:) - (Clearance.*vector2)/norm(vector2);
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC2_SurfCenter(1,:), pt2, pt3, EC3_SurfCenter(2,:),EC2_center, EC3_center, EC2, EC3, bauraum, cable_radius, radius_of_curvature);
+                Fitness(i,1) = Fitness(i,1) + Penalty_spline;
+                vector1 = EC1_SurfCenter(4,:) - EC1_SurfCenter(3,:);
+                vector2 = EC4_SurfCenter(3,:) - EC4_SurfCenter(4,:);
+                pt2 = EC1_SurfCenter(3,:) - (Clearance.*vector1)/norm(vector1);
+                pt3 = EC4_SurfCenter(4,:) - (Clearance.*vector2)/norm(vector2);
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(3,:), pt2, pt3, EC4_SurfCenter(4,:),EC1_center, EC4_center, EC1, EC4, bauraum, cable_radius, radius_of_curvature);
+                Fitness(i,1) = Fitness(i,1) + Penalty_spline;
 
-            %pt3 mid spline
-            vector_pt1 = [EC1_SurfCenter(2,1)+(-clearance_mid) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(2,3)];
-            vector_pt2 = EM_connection+[0 -clearance_mid 0]; 
-            vector_mid = vector_pt1 - vector_pt2;
-            vector_mid = vector_mid / norm(vector_mid);           
-            pt3 = EM_connection+[0 -clearance_mid 0]+ rad_gb*vector_mid;
-            pt6 = EM_connection + [cable_radius 0 0];
-            pt11 = EM_connection + [-cable_radius 0 0];
-            pt7 = EM_connection+[0 -clearance_mid 0] + [cable_radius 0 0];
-            pt12 = EM_connection+[0 -clearance_mid 0] + [-cable_radius 0 0];
-            pt10 = EC1_SurfCenter(2,:) + [0 EC1_width/4 0];
-            pt15 = EC1_SurfCenter(2,:) + [0 -EC1_width/4 0];
-            pt9 = pt10 + (clearance_posy.*vector1)/norm(vector1);
-            pt14 = pt15 + (clearance_negy.*vector1)/norm(vector1);
-            %pt8 pos y spline
-            vector_pt3 = [pt9(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(2,3)];
-            vector_pt4 = pt6 +[0 -clearance_mid 0]; 
-            vector_posy = vector_pt3 - vector_pt4;
-            vector_posy = vector_posy / norm(vector_posy);
-            pt8 = EM_connection+[0 -clearance_posy 0]+ rad_gb*vector_posy;
-            %pt13 neg y spline
-            vector_pt5 = [pt14(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(2,3)];
-            vector_pt6 = pt11 +[0 -clearance_mid 0]; 
-            vector_negy = vector_pt5 - vector_pt6;
-            vector_negy = vector_negy / norm(vector_negy);
-            pt13 = EM_connection+[0 -clearance_negy 0]+ rad_gb*vector_negy;
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwithgb,fn_mid,fn_posy,fn_negy] = spline_connection_gb (EM_connection, EM_connection+[0 -clearance_mid 0], pt3, EC1_SurfCenter(2,:)+(clearance_mid.*vector1)/norm(vector1), EC1_SurfCenter(2,:), pt6, pt7, pt8, pt9, pt10, pt11, pt12, pt13, pt14, pt15,EC1_center, EM_start, EM_end, gearbox, EC1,  cable_radius, radius_of_curvature);
-            
-        else                    %Clockwise
+                EM_connection = EM_end;
+                clearance_mid = 20;
+                clearance_posy = 30;
+                clearance_negy = 10;
+                rad_gb = EM_radius;
+                vector1 = EC1_SurfCenter(2,:) - EC1_SurfCenter(1,:);
 
-            vector1 = EC1_SurfCenter(1,:) - EC1_SurfCenter(2,:);
-            vector2 = EC2_SurfCenter(2,:) - EC2_SurfCenter(1,:);
-            pt2 = EC1_SurfCenter(2,:) - (Clearance.*vector1)/norm(vector1);
-            pt3 = EC2_SurfCenter(1,:) - (Clearance.*vector2)/norm(vector2);        
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(2,:), pt2, pt3, EC2_SurfCenter(1,:),EC1_center, EC2_center, EC1, EC2, cable_radius, radius_of_curvature);
-            Fitness(i,1) = Fitness(i,1) + Penalty_spline;
-            vector1 = EC2_SurfCenter(1,:) - EC2_SurfCenter(2,:);
-            vector2 = EC3_SurfCenter(2,:) - EC3_SurfCenter(1,:);
-            pt2 = EC2_SurfCenter(2,:) - (Clearance.*vector1)/norm(vector1);
-            pt3 = EC3_SurfCenter(1,:) - (Clearance.*vector2)/norm(vector2);        
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC2_SurfCenter(2,:), pt2, pt3, EC3_SurfCenter(1,:),EC2_center, EC3_center, EC2, EC3, cable_radius, radius_of_curvature);
-            Fitness(i,1) = Fitness(i,1) + Penalty_spline;
-            vector1 = EC1_SurfCenter(4,:) - EC1_SurfCenter(3,:);
-            vector2 = EC4_SurfCenter(3,:) - EC4_SurfCenter(4,:);
-            pt2 = EC1_SurfCenter(3,:) - (Clearance.*vector1)/norm(vector1);
-            pt3 = EC4_SurfCenter(4,:) - (Clearance.*vector2)/norm(vector2);        
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(3,:), pt2, pt3, EC4_SurfCenter(4,:),EC1_center, EC4_center, EC1, EC4, cable_radius, radius_of_curvature);
-            Fitness(i,1) = Fitness(i,1) + Penalty_spline;
-            
-            EM_connection = EM_end;
-            clearance_mid = 20;
-            clearance_posy = 30;
-            clearance_negy = 10;
-            rad_gb = EM_radius;
-            vector1 = EC1_SurfCenter(1,:) - EC1_SurfCenter(2,:);
+                %pt3 mid spline
+                vector_pt1 = [EC1_SurfCenter(2,1)+(-clearance_mid) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(2,3)];
+                vector_pt2 = EM_connection+[0 -clearance_mid 0]; 
+                vector_mid = vector_pt1 - vector_pt2;
+                vector_mid = vector_mid / norm(vector_mid);           
+                pt3 = EM_connection+[0 -clearance_mid 0]+ rad_gb*vector_mid;
+                pt6 = EM_connection + [cable_radius 0 0];
+                pt11 = EM_connection + [-cable_radius 0 0];
+                pt7 = EM_connection+[0 -clearance_mid 0] + [cable_radius 0 0];
+                pt12 = EM_connection+[0 -clearance_mid 0] + [-cable_radius 0 0];
+                pt10 = EC1_SurfCenter(2,:) + [0 EC1_width/4 0];
+                pt15 = EC1_SurfCenter(2,:) + [0 -EC1_width/4 0];
+                pt9 = pt10 + (clearance_posy.*vector1)/norm(vector1);
+                pt14 = pt15 + (clearance_negy.*vector1)/norm(vector1);
+                %pt8 pos y spline
+                vector_pt3 = [pt9(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(2,3)];
+                vector_pt4 = pt6 +[0 -clearance_mid 0]; 
+                vector_posy = vector_pt3 - vector_pt4;
+                vector_posy = vector_posy / norm(vector_posy);
+                pt8 = EM_connection+[0 -clearance_posy 0]+ rad_gb*vector_posy;
+                %pt13 neg y spline
+                vector_pt5 = [pt14(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(2,3)];
+                vector_pt6 = pt11 +[0 -clearance_mid 0]; 
+                vector_negy = vector_pt5 - vector_pt6;
+                vector_negy = vector_negy / norm(vector_negy);
+                pt13 = EM_connection+[0 -clearance_negy 0]+ rad_gb*vector_negy;
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwithgb,fn_mid,fn_posy,fn_negy] = spline_connection_gb (EM_connection, EM_connection+[0 -clearance_mid 0], pt3, EC1_SurfCenter(2,:)+(clearance_mid.*vector1)/norm(vector1), EC1_SurfCenter(2,:), pt6, pt7, pt8, pt9, pt10, pt11, pt12, pt13, pt14, pt15,EC1_center, EM_start, EM_end, gearbox, EC1,  cable_radius, radius_of_curvature);
 
-            %pt3 mid spline
-            vector_pt1 = [EC1_SurfCenter(1,1)+(clearance_mid) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(1,3)];
-            vector_pt2 = EM_connection+[0 -clearance_mid 0]; 
-            vector_mid = vector_pt1 - vector_pt2;
-            vector_mid = vector_mid / norm(vector_mid);           
-            pt3 = EM_connection+[0 -clearance_mid 0]+ rad_gb*vector_mid;
-            pt6 = EM_connection + [cable_radius 0 0];
-            pt11 = EM_connection + [-cable_radius 0 0];
-            pt7 = EM_connection+[0 -clearance_mid 0] + [cable_radius 0 0];
-            pt12 = EM_connection+[0 -clearance_mid 0] + [-cable_radius 0 0];
-            pt10 = EC1_SurfCenter(1,:) + [0 EC1_width/4 0];
-            pt15 = EC1_SurfCenter(1,:) + [0 -EC1_width/4 0];
-            pt9 = pt10 + (clearance_posy.*vector1)/norm(vector1);
-            pt14 = pt15 + (clearance_negy.*vector1)/norm(vector1);
-            %pt8 pos y spline
-            vector_pt3 = [pt9(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(1,3)];
-            vector_pt4 = pt6 +[0 -clearance_mid 0]; 
-            vector_posy = vector_pt3 - vector_pt4;
-            vector_posy = vector_posy / norm(vector_posy);
-            pt8 = EM_connection+[0 -clearance_posy 0]+ rad_gb*vector_posy;
-            %pt13 neg y spline
-            vector_pt5 = [pt14(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(1,3)];
-            vector_pt6 = pt11 +[0 -clearance_mid 0]; 
-            vector_negy = vector_pt5 - vector_pt6;
-            vector_negy = vector_negy / norm(vector_negy);
-            pt13 = EM_connection+[0 -clearance_negy 0]+ rad_gb*vector_negy;
-            [splinelength,max_curvature, curvature, collisionwith1, collisionwithgb,fn_mid,fn_posy,fn_negy] = spline_connection_gb (EM_connection, EM_connection+[0 -clearance_mid 0], pt3, EC1_SurfCenter(1,:)+(clearance_mid.*vector1)/norm(vector1), EC1_SurfCenter(1,:), pt6, pt7, pt8, pt9, pt10, pt11, pt12, pt13, pt14, pt15,EC1_center, EM_start, EM_end, gearbox, EC1, cable_radius, radius_of_curvature);
+            else                    %Clockwise
 
-        end
+                vector1 = EC1_SurfCenter(1,:) - EC1_SurfCenter(2,:);
+                vector2 = EC2_SurfCenter(2,:) - EC2_SurfCenter(1,:);
+                pt2 = EC1_SurfCenter(2,:) - (Clearance.*vector1)/norm(vector1);
+                pt3 = EC2_SurfCenter(1,:) - (Clearance.*vector2)/norm(vector2);        
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(2,:), pt2, pt3, EC2_SurfCenter(1,:),EC1_center, EC2_center, EC1, EC2, bauraum, cable_radius, radius_of_curvature);
+                Fitness(i,1) = Fitness(i,1) + Penalty_spline;
+                vector1 = EC2_SurfCenter(1,:) - EC2_SurfCenter(2,:);
+                vector2 = EC3_SurfCenter(2,:) - EC3_SurfCenter(1,:);
+                pt2 = EC2_SurfCenter(2,:) - (Clearance.*vector1)/norm(vector1);
+                pt3 = EC3_SurfCenter(1,:) - (Clearance.*vector2)/norm(vector2);        
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC2_SurfCenter(2,:), pt2, pt3, EC3_SurfCenter(1,:),EC2_center, EC3_center, EC2, EC3, bauraum, cable_radius, radius_of_curvature);
+                Fitness(i,1) = Fitness(i,1) + Penalty_spline;
+                vector1 = EC1_SurfCenter(4,:) - EC1_SurfCenter(3,:);
+                vector2 = EC4_SurfCenter(3,:) - EC4_SurfCenter(4,:);
+                pt2 = EC1_SurfCenter(3,:) - (Clearance.*vector1)/norm(vector1);
+                pt3 = EC4_SurfCenter(4,:) - (Clearance.*vector2)/norm(vector2);        
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwith2, Penalty_spline, fn] = spline_connection (EC1_SurfCenter(3,:), pt2, pt3, EC4_SurfCenter(4,:),EC1_center, EC4_center, EC1, EC4, bauraum, cable_radius, radius_of_curvature);
+                Fitness(i,1) = Fitness(i,1) + Penalty_spline;
+
+                EM_connection = EM_end;
+                clearance_mid = 20;
+                clearance_posy = 30;
+                clearance_negy = 10;
+                rad_gb = EM_radius;
+                vector1 = EC1_SurfCenter(1,:) - EC1_SurfCenter(2,:);
+
+                %pt3 mid spline
+                vector_pt1 = [EC1_SurfCenter(1,1)+(clearance_mid) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(1,3)];
+                vector_pt2 = EM_connection+[0 -clearance_mid 0]; 
+                vector_mid = vector_pt1 - vector_pt2;
+                vector_mid = vector_mid / norm(vector_mid);           
+                pt3 = EM_connection+[0 -clearance_mid 0]+ rad_gb*vector_mid;
+                pt6 = EM_connection + [cable_radius 0 0];
+                pt11 = EM_connection + [-cable_radius 0 0];
+                pt7 = EM_connection+[0 -clearance_mid 0] + [cable_radius 0 0];
+                pt12 = EM_connection+[0 -clearance_mid 0] + [-cable_radius 0 0];
+                pt10 = EC1_SurfCenter(1,:) + [0 EC1_width/4 0];
+                pt15 = EC1_SurfCenter(1,:) + [0 -EC1_width/4 0];
+                pt9 = pt10 + (clearance_posy.*vector1)/norm(vector1);
+                pt14 = pt15 + (clearance_negy.*vector1)/norm(vector1);
+                %pt8 pos y spline
+                vector_pt3 = [pt9(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(1,3)];
+                vector_pt4 = pt6 +[0 -clearance_mid 0]; 
+                vector_posy = vector_pt3 - vector_pt4;
+                vector_posy = vector_posy / norm(vector_posy);
+                pt8 = EM_connection+[0 -clearance_posy 0]+ rad_gb*vector_posy;
+                %pt13 neg y spline
+                vector_pt5 = [pt14(1) EM_connection(2)+(-clearance_mid) EC1_SurfCenter(1,3)];
+                vector_pt6 = pt11 +[0 -clearance_mid 0]; 
+                vector_negy = vector_pt5 - vector_pt6;
+                vector_negy = vector_negy / norm(vector_negy);
+                pt13 = EM_connection+[0 -clearance_negy 0]+ rad_gb*vector_negy;
+                [splinelength,max_curvature, curvature, collisionwith1, collisionwithgb,fn_mid,fn_posy,fn_negy] = spline_connection_gb (EM_connection, EM_connection+[0 -clearance_mid 0], pt3, EC1_SurfCenter(1,:)+(clearance_mid.*vector1)/norm(vector1), EC1_SurfCenter(1,:), pt6, pt7, pt8, pt9, pt10, pt11, pt12, pt13, pt14, pt15,EC1_center, EM_start, EM_end, gearbox, EC1, cable_radius, radius_of_curvature);
+
+            end
+         end
      end
     
     
